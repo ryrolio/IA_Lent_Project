32a2f958298115afd2f74b672807a3c63e754ce4
Ryan Tam <ryanztam27@gmail.com>
2022-01-26 00:17:31 +0000

    25/1

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,20 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+from utils import sorted_by_key  # noqa 
+from haversine import haversine, Unit 
+
+def stations_by_distance(stations,p):
+    """Returns a list of stations and distances, and sorts them by distance from a given co-ordinate"""
+
+    # Initialise a List 
+    list = []
+
+    for station in stations:
+        # Calculate distance from each station to the given coordinate
+        D = haversine(station.coord,p)
+
+        # Create a list of towns 
+        list.append((station.name,D))
+    
+    return sorted_by_key(list)
