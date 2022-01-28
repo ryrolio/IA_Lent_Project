21b1cb4a48ee8aedee616df3a643f8a4b0b747bf
amygibbon <98465727+amygibbon@users.noreply.github.com>
2022-01-28 13:10:22 +0000

    28/1

--- Task1E.py
+++ Task1E.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import rivers_with_station 
+from floodsystem.stationdata import build_station_list 
+from floodsystem.geo import stations_by_river 
+
+# Build a list of stations 
+stations = build_station_list()
+
+print(rivers_by_station_number(stations,11))
