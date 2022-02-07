e0a0b7c97099a58f002452c9498379dfd6a38d3e
Ryan Tam <ryanztam27@gmail.com>
2022-02-07 12:18:52 +0000

    7/2

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,1 +1,10 @@
-print("hellow world")
+"""This module contains a collection of functions related to flood data."""
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import * 
+
+### TASK 2B 
+stations = build_station_list() 
+
+def stations_level_over_threshold(stations, tol): 
+    """Returns a list of stations that have a relative water level over the threshold"""
