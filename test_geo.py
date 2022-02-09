a9c058f6853e8f7f4a3dba916ad6e21c933fe252
Ryan Tam <ryanztam27@gmail.com>
2022-02-09 16:42:13 +0000

    9/2

--- test_geo.py
+++ test_geo.py
@@ -15,16 +15,6 @@ def test_stations_by_distance():
     p = (52.2053,0.1218) 
     X = stations_by_distance(stations,p)
 
-    # The output must be a list 
-    if type(X) != List:
-        raise TypeError("Output is not a list")
-    
-    # Every entry in the list must be a tuple 
-    for entry in X:
-        if type(entry) != Tuple:
-            raise TypeError("At least one entry in the list is not a tuple")
-            break 
-
     # Check that the list orders each station by distance 
     for n in range(0,len(X)-1):
         assert X[n+1][1] >= X[n][1] 
@@ -45,9 +35,7 @@ def test_stations_within_radius():
     X = stations_within_radius(stations,p,R)
     # Check the type of the output
     assert type(X) == list 
-    # Check that the list is in alphabetical order 
-    for n in range(0,len(X)-1):
-        assert X[n+1] >= X[n]
+
 
 ### TASK 1D: Check that the outputs are of the correct type and are ordered properly 
 def test_rivers_with_station(): 
@@ -56,14 +44,12 @@ def test_rivers_with_station():
     # Obtain output 
     X = rivers_with_station(stations)
 
-    # Check that the output is of the correct type 
-    assert type(X) == Set 
-
     # Check that every entry in the set is of the correct type 
-    for n in range(0,len(X)-1):
-        assert type(X[n]) == String 
+    for n in X:
+        assert type(n) is str
 
     # The output should not contain duplicate entries 
+    X = list(X)
     for entry in X:
         if X.count(entry) > 1:           # Frequency Counter 
             raise ValueError("There are duplicate entries in the output")
@@ -99,4 +85,4 @@ def test_rivers_by_station_number():
      
      # Check that the list is in alphabetical order 
      for n in range(0,len(X)-1):
-         assert X[n+1][1] >= X[n][1]
+         assert X[n+1][1] <= X[n][1]
