9f95665f1b4a2fbf09e9108cac36860b9378fc98
amygibbon <98465727+amygibbon@users.noreply.github.com>
2022-02-21 12:24:07 +0000

    Update analysis.py

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -8,19 +8,18 @@ x = matplotlib.dates.date2num(dates)
 def polyfit(dates, levels, p):
   """Given the water level time history, the function computes a least-squares fit of a polynomial of degree p to water level data, returning a tuple of the polynomial object and any shift of the time axis"""
   
-  # Create set of 10 data points on interval (1000, 1002)
-  dates = x = np.linspace(10000, 10002, 10)
-  y = [0.1, 0.09, 0.23, 0.34, 0.78, 0.74, 0.43, 0.31, 0.01, -0.05]
-
-  # Using shifted x values, find coefficient of best-fit
-  # polynomial f(x) of degree 4
-  p_coeff = np.polyfit(x - x[0], y, 4)
-
-  # Convert coefficient into a polynomial that can be evaluated
-  # e.g. poly(0.3)
-  poly = np.poly1d(p_coeff)
-
-  poly, d0 = polyfit(dates, levels, 3)
+  float_dates = matplotlib.dates.date2num(dates)
+  
+  d0 = float_dates[0]
+  shift_dates = []
+  for i in range(len(float_dates)):
+    shift_dates.append(float_dates[i] - float_dates[0])
+  
+  coefficient = np.polyfit(shift_dates, levels, p)
+  
+  poly = np.poly1d(coefficient)
+  
+  return (poly, d0)
 
 
 
