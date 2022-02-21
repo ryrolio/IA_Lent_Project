363e1e8bc15f8eb4e981b6ce9d19d820f6b4ac36
amygibbon <98465727+amygibbon@users.noreply.github.com>
2022-02-21 12:36:02 +0000

    Update plot.py

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt 
 from datetime import datetime, timedelta 
 import numpy as np 
+from floodsystem.analysis import *
 
 ### TASK 2E
 def plot_water_levels(station, dates, levels):
@@ -26,7 +27,40 @@ def plot_water_levels(station, dates, levels):
     plt.title(station.name + " Water Level")
     plt.tight_layout() 
 
-    plt.show()
+    return plt.show()
  
 ### TASK 2F
 def plot_water_level_with_fit(station, dates, levels, p):
+    """Plots the water level data and best fit polynomial"""
+    float_dates = matplotlib.dates.date2num(dates)
+
+    d0 = float_dates[0]
+    shift_dates = []
+    for i in range(len(float_dates)):
+        shift_dates.append(float_dates[i] - float_dates[0])
+  
+    coefficient = np.polyfit(shift_dates, levels, p)
+  
+    poly = np.poly1d(coefficient)
+    
+    plt.plot(shift_dates, levels, '.')
+    
+    # Obtain y-axis data 
+    high_level = [station.typical_range[1]]*len(dates) 
+    low_level = [station.typical_range[0]]*len(dates)
+
+    # Plot the data 
+    plt.plot(dates, levels, label = "Water Level")
+    plt.plot(dates, high_level, label = "Typical High Level")
+    plt.plot(dates, low_level, label = "Typical Low Level")
+
+    # Add labels/title/legend 
+    plt.xlabel("Date")
+    plt.ylabel("water Level (m)")
+    plt.legend()
+    plt.xticks(rotation = 45) 
+    plt.title(station.name + " Water Level")
+    plt.tight_layout() 
+
+    return plt.show()
+    
