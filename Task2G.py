0f9ab52b1b8fa1c980275a7189dbc217a4ddd20f
Ryan Tam <ryanztam27@gmail.com>
2022-02-22 12:38:10 +0000

    22/2

--- Task2G.py
+++ Task2G.py
@@ -1,13 +1,10 @@
 ### This file uses existing implmentations to decide which regions are at the
 ### highest risk of flooding.
 
-<<<<<<< HEAD
 from floodsystem.analysis import * 
 from floodsystem.datafetcher import * 
 from floodsystem.stationdata import * 
 import datetime
-=======
->>>>>>> 036cfb1f70c9525e2854bf79eb55c5fd387c758c
 
 stations = build_station_list()
 update_water_levels(stations)
@@ -23,24 +20,27 @@ low_risk = []
 
 for station in stations:
    #fetch measure levels
-   dates, levels = fetch_measure_levels(station.measure.id, dt = datetime.timedelta(days = dt)
+   dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
+
+   if type(station.relative_water_level()) == type(None):
+       continue 
                                         
-   if station.relative_water_level() > 1.5:
+   elif station.relative_water_level() > 1.5:
       severe_risk.append((station.name, station.relative_water_level()))
                                         
-   elif station.relative_water_level() > 1 and rising_check(stations, levels, p) == True:
+   elif station.relative_water_level() > 1 and rising_check(dates, levels, p) == True:
       severe_risk.append((station.name, station.relative_water_level()))
                                         
    elif station.relative_water_level() > 1:
       high_risk.append((station.name, station.relative_water_level()))
                                         
-   elif station.relative_water_level() > 0.5 and rising_check(stations, levels, p) == True:
+   elif station.relative_water_level() > 0.5 and rising_check(dates, levels, p) == True:
       high_risk.append((station.name, station.relative_water_level()))
                                         
    elif station.relative_water_level() > 0.5:
       moderate_risk.append((station.name, station.relative_water_level()))
                                         
-   elif station.relative_water_level() > 0 and rising_check(stations, levels, p) == True:
+   elif station.relative_water_level() > 0 and rising_check(dates, levels, p) == True:
       moderate_risk.append((station.name, station.relative_water_level()))
                                         
    elif station.relative_water_level() <= 0:
