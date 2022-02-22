ee76205e2570a36324d5dd4289c65b6e45ad66cd
amygibbon <98465727+amygibbon@users.noreply.github.com>
2022-02-22 11:11:47 +0000

    Update Task2G.py

--- Task2G.py
+++ Task2G.py
@@ -1,2 +1,15 @@
-### This file uses all existing implmentations to decide which regions are at the
-### highest risk of flooding. 
+### This file uses existing implmentations to decide which regions are at the
+### highest risk of flooding.
+
+stations = build_station_list()
+update_water_levels(stations)
+
+severe_risk = []
+high_risk = []
+moderate_risk = []
+low_risk = []
+
+for station in stations_level_over_threshold(stations, 1.5):
+  severe_risk
+  
+  
