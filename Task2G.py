ab89a2262fd27e460cc194ad7b58aad111d60515
amygibbon <98465727+amygibbon@users.noreply.github.com>
2022-02-22 11:31:28 +0000

    Update Task2G.py

--- Task2G.py
+++ Task2G.py
@@ -9,7 +9,41 @@ high_risk = []
 moderate_risk = []
 low_risk = []
 
+
+
 for station in stations_level_over_threshold(stations, 1.5):
-  severe_risk
-  
+   if rising_check(stations) = True:
+    severe_risk.append((station.name, station.relative_water_level))
+   else:
+    high_risk.append((station.name, station.relative_water_level))
+
+for station in stations_level_over_threshold(stations, 1):
+   if rising_check(stations) = True:
+    high_risk.append((station.name, station.relative_water_level))
+   else:
+    moderate_risk.append((station.name, station.relative_water_level))
+    
+for station in stations_level_over_threshold(stations, 0.5):
+   if rising_check(stations) = True:
+    moderate_risk.append((station.name, station.relative_water_level))
+   else:
+    low_risk.append((station.name, station.relative_water_level))
+
+for station in stations_level_over_threshold(stations, 0):
+   if rising_check(stations) = True:
+    low_risk.append((station.name, station.relative_water_level))
+   else:
+    low_risk.append((station.name, station.relative_water_level))
+
+    
+s = set(severe_risk)
+h = set(high_risk)
+m = set(moderate_risk)
+l = set(low_risk)
+
+severe_risk = list(s)
+high_risk = list(h)
+moderate_risk = list(m)
+low_risk = list(l)
+
   
