9023dfb199b5db7676fef61f0fca46ab69707461
Ryan Tam <ryanztam27@gmail.com>
2022-03-09 23:50:43 +0000

    3/3

--- Task2G.py
+++ Task2G.py
@@ -25,18 +25,49 @@ for station in stations:
       if float(station.relative_water_level()) >= 1.5 and float(rising_check(station, 4)) > 0:      
          # Classed as 'severe': high relative level and rising levels 
          severe_risk_towns.append(station.town) 
+         
+         
       
       elif float(station.relative_water_level()) >= 1.5 and float(rising_check(station, 4)) <= 0:    
          # Classed as 'high': high relative levels but levels are not rising
          high_risk_towns.append(station.town) 
+         
+         
       
       elif 0.75 <= float(station.relative_water_level()) < 1.5:  # Classed as 'moderate' 
          moderate_risk_towns.append(station.town) 
+         
+         
       
       elif float(station.relative_water_level()) < 0.75:     # Classed as 'low' 
          low_risk_towns.append(station.town)
-  
+         
+         
+severe_risk_set = set(severe_risk_towns)
+high_risk_set = set(high_risk_towns)
+moderate_risk_set = set(moderate_risk_towns)
+low_risk_set = set(low_risk_towns)
+
+high_risk_towns = high_risk_set.difference(severe_risk_set) 
+high_risk_towns = high_risk_set.difference(moderate_risk_set) 
+high_risk_towns = high_risk_set.difference(low_risk_set) 
+
+moderate_risk_towns = moderate_risk_set.difference(severe_risk_set) 
+moderate_risk_towns = moderate_risk_set.difference(high_risk_set) 
+moderate_risk_towns = moderate_risk_set.difference(low_risk_set) 
+
+low_risk_towns = low_risk_set.difference(severe_risk_set) 
+low_risk_towns = low_risk_set.difference(moderate_risk_set) 
+low_risk_towns = low_risk_set.difference(high_risk_set) 
+
 # Return the final output 
+severe_risk_towns = list(severe_risk_set) 
+low_risk_towns = list(low_risk_set)
+moderate_risk_towns = list(moderate_risk_set)
+high_risk_towns = list(high_risk_set) 
+
+
+
 all_towns = [severe_risk_towns, high_risk_towns, moderate_risk_towns, low_risk_towns]
 
 severe_towns = all_towns[0] 
